78e1ee87cfde9f5ad458aaf5def65a8840be81f6
Tyler Pooschke <tyler.pooschke@pm.me>
2022-05-11 12:35:42 -0500

    section 8

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./compenents/layout/Footer";
+import Navbar from "./compenents/layout/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
 function App() {
 	return (
-		<div>
-			<h1>hello world</h1>
-		</div>
+		<Router>
+			<div className="flex flex-col justify-between h-screen">
+				<Navbar />
+				<main className="container mx-auto px-3 pb-12">
+					<Routes>
+						<Route path="/" element={<Home />}></Route>
+						<Route path="/about" element={<About />}></Route>
+						<Route path="/notfound" element={<NotFound />}></Route>
+						<Route path="/*" element={<NotFound />}></Route>
+					</Routes>
+				</main>
+				<Footer />
+			</div>
+		</Router>
 	);
 }
 
